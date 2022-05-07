80c7513e9be3796edaf0e3d8271b4bfdbde31ba3
sababot <twinsgardos@gmail.com>
2022-05-07 13:54:33 +0200

    first commit (already have a lot of progress)

---
+++ build/CMakeFiles/VulkanTutorial.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VulkanTutorial.dir/main.cpp.o"
+  "CMakeFiles/VulkanTutorial.dir/main.cpp.o.d"
+  "VulkanTutorial"
+  "VulkanTutorial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VulkanTutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/VulkanTutorial.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for VulkanTutorial.
+# This may be replaced when dependencies are built.
